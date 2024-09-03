52ae541ef03b8e910fc698cbefde6531e1a6ae26
Roman Kukla <kukla.roman@gmail.com>
2024-09-03 22:52:23 +0300

    Added auth controller

---
+++ controllers/authControllers.js
@@ -0,0 +1,28 @@
+import gravatar from "gravatar";
+import * as usersServices from "../services/usersServices.js";
+import ctrlWrapper from "../middleware/ctrlWrapper.js";
+import "dotenv/config";
+
+const signup = async (req, res, next) => {
+  try {
+    const {...userData} = req.body;
+    const gravatarUrl = gravatar.url(userData.email, {s: "250"}, true);
+    const newUser = await usersServices.signup({...userData, avatarURL: gravatarUrl,});
+
+    res.status(201).json({
+      user: {
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    if (error?.message === "Email in use") {
+      return res.status(409).json({message: error.message});
+    }
+    next(error);
+  }
+};
+
+export default {
+  signup: ctrlWrapper(signup),
+};
