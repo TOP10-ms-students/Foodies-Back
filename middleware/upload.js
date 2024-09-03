40fad4c436288d4068d91ece2d5c1e6571f6f1d2
Roman Kukla <kukla.roman@gmail.com>
2024-09-04 01:17:08 +0300

    Fixed codestyle

--- middleware/upload.js
+++ middleware/upload.js
@@ -5,24 +5,24 @@ import {ApiError} from "../errors/apiError.js";
 const destination = path.resolve("temp");
 
 const storage = multer.diskStorage({
-  destination,
-  filename: (req, file, callback) => {
-    const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const filename = `${uniquePrefix}_${file.originalname}`;
-    callback(null, filename);
-  },
+    destination,
+    filename: (req, file, callback) => {
+        const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const filename = `${uniquePrefix}_${file.originalname}`;
+        callback(null, filename);
+    },
 });
 
 const limits = {
-  fileSize: 1024 * 1024 * 5,
+    fileSize: 1024 * 1024 * 5,
 };
 
 const fileFilter = (req, file, callback) => {
-  const extension = file.originalname.split(".").pop();
-  if (extension === "exe") {
-    return callback(ApiError(400, ".exe extension not allowed"));
-  }
-  callback(null, true);
+    const extension = file.originalname.split(".").pop();
+    if (extension === "exe") {
+        return callback(ApiError(400, ".exe extension not allowed"));
+    }
+    callback(null, true);
 };
 
 const upload = multer({storage, limits, fileFilter});
--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 export const signupSchema = Joi.object({
-  name: Joi.string().min(2).max(30).required(),
-  email: Joi.string().email().max(100).required(),
-  password: Joi.string().min(5).required(),
-  avatarURL: Joi.string().optional(),
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().max(100).required(),
+    password: Joi.string().min(5).required(),
+    avatarURL: Joi.string().optional(),
 });
