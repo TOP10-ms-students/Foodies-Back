962a91ef2d3cbc41b3ad1aca49d4e6aff810467e
Roman Kukla <kukla.roman@gmail.com>
2024-09-03 22:54:35 +0300

    Added upload middleware

---
+++ middleware/upload.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import path from "node:path";
+import {ApiError} from "../errors/apiError.js";
+
+const destination = path.resolve("temp");
+
+const storage = multer.diskStorage({
+  destination,
+  filename: (req, file, callback) => {
+    const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const filename = `${uniquePrefix}_${file.originalname}`;
+    callback(null, filename);
+  },
+});
+
+const limits = {
+  fileSize: 1024 * 1024 * 5,
+};
+
+const fileFilter = (req, file, callback) => {
+  const extension = file.originalname.split(".").pop();
+  if (extension === "exe") {
+    return callback(ApiError(400, ".exe extension not allowed"));
+  }
+  callback(null, true);
+};
+
+const upload = multer({storage, limits, fileFilter});
+
+export default upload;
