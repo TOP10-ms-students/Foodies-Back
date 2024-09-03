53b41c6e9086a17496b349ebae10f67b21a5b87f
Roman Kukla <kukla.roman@gmail.com>
2024-09-04 00:43:41 +0300

    Removed email regexp from user model

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,9 +1,8 @@
 import Joi from "joi";
-import { emailRegExp } from "../constants/constants.js";
 
 export const signupSchema = Joi.object({
   name: Joi.string().min(2).max(30).required(),
-  email: Joi.string().pattern(emailRegExp).required(),
+  email: Joi.string().email().max(100).required(),
   password: Joi.string().min(5).required(),
   avatarURL: Joi.string().optional(),
 });
