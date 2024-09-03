1e2df2978e5aa7208bb68d3472d9450cfa4f1a22
Roman Kukla <kukla.roman@gmail.com>
2024-09-04 00:40:52 +0300

    Fixed Users.create

--- services/usersServices.js
+++ services/usersServices.js
@@ -1,16 +1,17 @@
 import Users from "../db/models/users.cjs";
+import db from "../db/models/index.cjs";
 import bcrypt from "bcrypt";
 
 export const findUser = (query) => Users.findOne(query);
 export const signup = async (data) => {
-  try {
-    const {password} = data;
-    const hashPassword = await bcrypt.hash(password, 10);
-    return await Users.create({...data, password: hashPassword});
-  } catch (error) {
-    if (error?.parent?.code === "23505") {
-      error.message = "Email in use";
+    try {
+        const {password} = data;
+        const hashPassword = await bcrypt.hash(password, 10);
+        return await db.Users.create({...data, password: hashPassword});
+    } catch (error) {
+        if (error?.parent?.code === "23505") {
+            error.message = "Email in use";
+        }
+        throw error;
     }
-    throw error;
-  }
 };
