32a45cf55725b90f1c5dd51d94e1e1f57ceb9d3f
Roman Kukla <kukla.roman@gmail.com>
2024-09-04 11:47:03 +0300

    Added current user route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,15 +3,17 @@ import authControllers from "../controllers/authControllers.js";
 import validateBody from "../middleware/validateBody.js";
 import {signupSchema, loginSchema} from "../schemas/usersSchemas.js";
 import upload from "../middleware/upload.js";
+import authenticate from "../middleware/authenticate.js";
 
 const validateRequestBody = validateBody(signupSchema);
 const validateLoginBody = validateBody(loginSchema);
-
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", upload.single("avatar"), validateRequestBody, authControllers.signup);
 
 authRouter.post("/login", validateLoginBody, authControllers.logIn);
+
+authRouter.get("/current", authenticate, authControllers.getCurrentUser);
 
 export default authRouter;
