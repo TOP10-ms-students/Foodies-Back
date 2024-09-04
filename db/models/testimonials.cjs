a66b40d68e883f27e33643d7381f2a15ffd81055
Roman Kukla <kukla.roman@gmail.com>
2024-09-05 00:35:54 +0300

    Added relation for testimonial

--- db/models/testimonials.cjs
+++ db/models/testimonials.cjs
@@ -8,12 +8,18 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            // define association here
             models.Users.hasMany(models.Testimonials, {
                 foreignKey: {
                     name: "owner",
                     allowNull: false,
                 },
+            });
+            models.Testimonials.belongsTo(models.Users, {
+                foreignKey: {
+                    name: "owner",
+                    allowNull: false,
+                },
+                onDelete: "CASCADE",
             });
         }
     }
