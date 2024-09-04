cf776392374c242d0f0ee0e210d14f7f1cd3af75
Maksym-Zyza <mmziza88@gmail.com>
2024-09-04 17:28:23 +0300

    added get all recipes, get one recipe

--- routes/recipes-router.js
+++ routes/recipes-router.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import authenticate from "../middleware/authenticate.js";
+import recipesControllers from "../controllers/recipesControllers.js";
 
-const router = Router();
+const recipesRouter = Router();
 
-export default router;
+recipesRouter.use(authenticate);
+
+recipesRouter.get("/", recipesControllers.getAllRecipes);
+
+recipesRouter.get("/:id", recipesControllers.getOneRecipe);
+
+export default recipesRouter;
