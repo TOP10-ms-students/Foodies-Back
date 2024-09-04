5bd2823ee80041e566c7d01d9364f4de875511cb
Filip <137682962+Filip-Povidernyi@users.noreply.github.com>
2024-09-04 12:37:34 +0300

    categories, areas endpoints
    Додав endpoints /categories та /areas

--- routes/areas-router.js
+++ routes/areas-router.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import db from "../db/models/index.cjs";
+import ctrlWrapper from "../middleware/ctrlWrapper.js";
 
 const router = Router();
 
+async function getAllAreas(_, res) {
+    const allAreas = await db.Areas.findAll();
+    res.json(allAreas);
+};
+
+router.get("/", ctrlWrapper(getAllAreas));
+
 export default router;
+
--- routes/categories-router.js
+++ routes/categories-router.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import db from "../db/models/index.cjs";
+import ctrlWrapper from "../middleware/ctrlWrapper.js";
 
 const router = Router();
 
+async function getAllCategories(_, res) {
+    const allCategories = await db.Categories.findAll();
+    res.json(allCategories);
+};
+
+router.get("/", ctrlWrapper(getAllCategories));
+
 export default router;
+
