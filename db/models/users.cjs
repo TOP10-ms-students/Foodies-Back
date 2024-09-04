7605148b0d761a3e7eb415a6675c81b2b4a75876
Roman Kukla <kukla.roman@gmail.com>
2024-09-04 11:46:41 +0300

    Added token column to users table

--- db/models/users.cjs
+++ db/models/users.cjs
@@ -26,6 +26,7 @@ module.exports = (sequelize, DataTypes) => {
                 unique: true,
             },
             password: DataTypes.STRING,
+            token: DataTypes.STRING,
         },
         {
             sequelize,
