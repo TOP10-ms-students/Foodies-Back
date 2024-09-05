12758f5773ce6174ed4c1d5708be2ba87f6f4f4d
Natalia <icxodnik@gmail.com>
2024-09-05 03:04:38 +0200

    generate openAPI documentation from our express handlers
    this is a hack which relies on internal express implementation.
    `ctrlWrapper` and `validateBody` has been extended to improve docs generation.

--- middleware/validateBody.js
+++ middleware/validateBody.js
@@ -1,6 +1,8 @@
 import { ApiError } from "../errors/apiError.js";
 
-const validateBody = (schema) => {
+export const schemaKey = Symbol("schema");
+
+const validateBody = schema => {
     const func = (req, _, next) => {
         const { error, value } = schema.validate(req.body, { allowUnknown: true, stripUnknown: true });
         if (error) {
@@ -9,6 +11,8 @@ const validateBody = (schema) => {
         req.body = value;
         next();
     };
+
+    func[schemaKey] = schema;
 
     return func;
 };
