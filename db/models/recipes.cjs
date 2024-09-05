bee1ce7b19c7810a4a090e4078dc95fc1a6f5045
Roman Kukla <kukla.roman@gmail.com>
2024-09-05 15:31:32 +0300

    Removed comment

--- db/models/recipes.cjs
+++ db/models/recipes.cjs
@@ -8,7 +8,6 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            // define association here
             models.Users.hasMany(models.Recipes, {
                 foreignKey: {
                     name: "owner",
