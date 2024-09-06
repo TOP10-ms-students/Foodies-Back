a859dd3ca817f456a92ae94bdddcddbcdc84818b
DmytroZhuk <dm.zhuk@gmail.com>
2024-09-06 18:47:21 +0300

    favoriteRecipesServices.js - .findOne() omitted

--- services/favoriteRecipesServices.js
+++ services/favoriteRecipesServices.js
@@ -1,10 +1,6 @@
 import db from "../db/models/index.cjs";
 
 const removeFavoriteRecipe = async ({ id, owner }) => {
-    await db.FavoriteRecipes.findOne({
-        where: { recipeId: id, userId: owner },
-    });
-
     await db.FavoriteRecipes.destroy({
         where: { recipeId: id, userId: owner },
     });
