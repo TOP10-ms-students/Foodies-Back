b5b0bde29aa755a21be7d6b185d90fab3de8aca3
Roman Kukla <kukla.roman@gmail.com>
2024-09-06 15:33:56 +0300

    Fixed conflict

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import authenticate from "../middleware/authenticate.js";
 import followersContr from "../controllers/followersController.js";
+import upload from "../middleware/upload.js";
+import usersController from "../controllers/usersController.js";
 
 const router = Router();
 
@@ -9,5 +11,7 @@ router.use(authenticate);
 router.post("/:id/follower", followersContr.addNewFollower);
 
 router.delete("/:id/follower", followersContr.deleteFollower);
+
+router.patch("/avatar", upload.single("avatar"), usersController.addAvatar);
 
 export default router;
