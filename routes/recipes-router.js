8650ea20d25b2a415dc674f3271ca499f00396cd
DmytroZhuk <dm.zhuk@gmail.com>
2024-09-06 11:38:37 +0300

    updated erro handling in deleteFavoriteRecipe

--- routes/recipes-router.js
+++ routes/recipes-router.js
@@ -4,6 +4,6 @@ import favoriteRecipesControllers from "../controllers/favoriteRecipesController
 
 const recipesRouter = Router();
 
-recipesRouter.delete("/favorite/:id", authenticate, recipesControllers.deleteFavoriteRecipe);
+recipesRouter.delete(":id/favorite", authenticate, recipesControllers.deleteFavoriteRecipe);
 
 export default recipesRouter;
--- services/recipesServices.js
+++ services/recipesServices.js
@@ -1,16 +1,20 @@
 import db from "../db/models/index.cjs";
+import { AppError, errorTypes } from "../errors/appError.js";
 
 const removeFavoriteRecipe = async ({ id, owner }) => {
     const existingFavorite = await db.FavoriteRecipes.findOne({
-        where: { recipeId: id, userId: owner },
-        rejectOnEmpty: true,
+        where: { recipe_id: id, user_id: owner },
     });
+
+    if (!existingFavorite) {
+        throw new AppError(errorTypes.NOT_FOUND, `Favorite recipe with id ${id} not found for user ${owner}`);
+    }
 
     await db.FavoriteRecipes.destroy({
-        where: { recipeId: id, userId: owner },
+        where: { recipe_id: id, user_id: owner },
     });
 
-    return true;
+    return { message: `Favorite recipe with id ${id} removed successfully` };
 };
 
 export default {
