122e1e95e6d346d5fcda35cf9b13c4f1829bb257
Filip-Povidernyi <p.poviderniy@gmail.com>
2024-09-08 12:07:03 +0300

    Update followersServices.js

--- services/followersServices.js
+++ services/followersServices.js
@@ -10,7 +10,7 @@ const getFollowers = (userId) => {
 
 const getFollowList = (userId) => {
     const follows = db.Followers.findAll({
-        where: { followerId: "64c8d958249fae54bae90bb9" },
+        where: { followerId: userId },
         attributes: ['userId'],
     });
     return follows;
